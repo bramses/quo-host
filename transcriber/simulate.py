bc9183da18a241bef2027703c9d13f382c29264e
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-03-14 22:23:42 -0400

    Refactor quote selection and add API call***

--- transcriber/simulate.py
+++ transcriber/simulate.py
@@ -37,13 +37,19 @@ def simulate_transcription_process():
         response_json = response.json()
         run = add_step(run, 'search', process_id, response_json)
         # get the quotes from the response
-        chosen_quote = choose_quote(response_json, section)
+        # filter out quotes that have already been shown
+        filtered_quotes = [quote for quote in response_json if quote['id'] not in run['quotes_shown']]
+        chosen_quote = choose_quote(filtered_quotes, section)
         run = add_step(run, 'select', process_id, chosen_quote)
+        # add quote id to quotes_shown
+        run = add_quote_shown(run, response_json[chosen_quote['index']]['id'])
         # bold the quote
         bolded_quote = bold_quote(response_json[chosen_quote['index']]['text'], section, chosen_quote['reasoning'])
         run = add_step(run, 'bold', process_id, bolded_quote)
+        # post to http://localhost:3000/new-quote-data with { text: bolded_quote, author: response_json[chosen_quote['index']]['author'], title: response_json[chosen_quote['index']]['title'] }
+        response = requests.post('http://localhost:3000/new-quote-data', json={'text': bolded_quote, 'author': response_json[chosen_quote['index']]['author'], 'title': response_json[chosen_quote['index']]['title']})
         # print(response.json())
-        # time.sleep(5)
+        time.sleep(5)
         idx += 1
     run = package_run(run)
     write_to_log(run, run_id=run['id'])
